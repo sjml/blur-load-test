dc3e7870cd0dfbc192dd1b56c34ec5b3ee2db4e8
Shane Liesegang <shane@techie.net>
2021-02-02 00:45:18 +0200

    readme to match

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# blur-load-test
+# Blur Load Tester
 
-explain this whole **deal**
+I was intrigued by the way [Medium](https://jmperezperez.com/medium-image-progressive-loading-placeholder/) and [Facebook](https://engineering.fb.com/2015/08/06/android/the-technology-behind-preview-photos/) use blurred
+preview images while the larger versions are loading. It's pretty clever
+— with a small enough preview image, it becomes reasonable to send it as
+a data element in the HTML itself instead of triggering another load.
+And with enough blurring, the artifacts from the super small image file
+aren't visible.
+
+So I ended up curious about optimal image compression for such a thing.
+I figured it would be different at different display sizes of the actual
+image, but wanted to see for myself. JPEG is not the ideal choice for
+images this small — it has a fixed overhead that usually is negligible
+but becomes a real factor when you're looking at just a few hundred
+bytes. WebP is actually great, getting down to just a hundred bytes or
+so with decent visual quality, but it's only supported in Safari on
+macOS &gt;= Big Sur. 😕 Other formats might be promising, but nothing has
+as broad support as JPEG. So here we are.
+
+Facebook went through the effort of identifying the common sections of
+the compressed JPEG header so they could save an additional 20 bytes or
+so… I'm not quite that interested in hyper-optimization, but of course
+there's almost always a little more space to trim.
+
+This page doesn't do any checking about what image types your browser
+supports, so if you choose one of the exotic ones and all the blurred
+images disappear, that's why.
+
+These photos were all taken by me at various points around the world.
+Consider them Creative Commons licensed ([CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/)), I guess.
--- src/App.svelte
+++ src/App.svelte
@@ -67,26 +67,26 @@
     <p>
         I was intrigued by the way <a href="https://jmperezperez.com/medium-image-progressive-loading-placeholder/">
         Medium</a> and <a href="https://engineering.fb.com/2015/08/06/android/the-technology-behind-preview-photos/">
-        Facebook</a> use blurred preview images while the larger versions are loading. It's pretty clever — with a
+        Facebook</a> use blurred preview images while the larger versions are loading. It’s pretty clever — with a
         small enough preview image, it becomes reasonable to send it as a data element in the HTML itself instead of
-        triggering another load. And with enough blurring, the artifacts from the super small image file aren't visible.
+        triggering another load. And with enough blurring, the artifacts from the super small image file aren’t visible.
     </p>
     <p>
         So I ended up curious about optimal image compression for such a thing. I figured it would be different at
         different display sizes of the actual image, but wanted to see for myself. JPEG is not the ideal choice for
-        images this small — it has a fixed overhead that usually is negligible but becomes a real factor when you're
+        images this small — it has a fixed overhead that usually is negligible but becomes a real factor when you’re
         looking at just a few hundred bytes. WebP is actually great, getting down to just a hundred bytes or so with
-        decent visual quality, but it's only supported in Safari on macOS >= Big Sur. 😕 Other formats might be
+        decent visual quality, but it’s only supported in Safari on macOS >= Big Sur. 😕 Other formats might be
         promising, but nothing has as broad support as JPEG. So here we are.
     </p>
     <p>
         Facebook went through the effort of identifying the common sections of the compressed JPEG header so they
-        could save an additional 20 bytes or so… I'm not quite that interested in hyper-optimization, but of course
-        there's almost always a little more space to trim.
+        could save an additional 20 bytes or so… I’m not quite that interested in hyper-optimization, but of course
+        there’s almost always a little more space to trim.
     </p>
     <p>
-        This page doesn't do any checking about what image types your browser supports, so if you choose one of the
-        exotic ones and all the blurred images disappear, that's why.
+        This page doesn’t do any checking about what image types your browser supports, so if you choose one of the
+        exotic ones and all the blurred images disappear, that’s why.
     </p>
     <p>
         These photos were all taken by me at various points around the world. Consider them Creative Commons
